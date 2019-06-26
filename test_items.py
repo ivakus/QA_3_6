b3064ba732fb8dbef84fb19d536aff85bb504746
d.ivaka <ivakus@mail.ru>
2019-06-26 17:38:25 +0300

    Исправлен запуск браузера

--- test_items.py
+++ test_items.py
@@ -6,8 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 
-def test_buttom_put_in_bascket (browser,lang):
-    link = "http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(lang)
+def test_buttom_put_in_bascket (browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     b_ = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket")))
     b_.click()
